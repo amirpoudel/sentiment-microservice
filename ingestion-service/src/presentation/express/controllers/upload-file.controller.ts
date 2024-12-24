29b94330897fb73b7691b4d841c83c09eab5a650
amirpoudel <amirpoudel2058@gmail.com>
2024-12-24 11:46:33 +0545

    file processing work done in ingestion

--- ingestion-service/src/presentation/express/controllers/upload-file.controller.ts
+++ ingestion-service/src/presentation/express/controllers/upload-file.controller.ts
@@ -2,24 +2,25 @@ import ApiResponse from "../../../lib/api/response.api";
 import asyncHandler from "../../../lib/async/express.async";
 import { Request,Response } from "express";
 import { AppError } from "../../../lib/error/app.error";
+import { IFileProcessingService } from "../../../interface/fileProcessing.interface";
+
 
 
 export class UploadFileController {
-
-    constructor(){
-
+    private fileProcessingService;
+    constructor(fileProcessingService:IFileProcessingService){
+        this.fileProcessingService = fileProcessingService
     }
 
     upload = asyncHandler(async(req:Request,res:Response)=>{
-        console.log(req);
+   
         const filePath = req.file?.path;
         if(!filePath){
             throw AppError.badRequest("Unable to upload file ! please try again");
         }
 
         // after success fully upload. call the stream for process
-
-        
+        this.fileProcessingService.processCSVFile(filePath);
 
         return res.status(200).json(new ApiResponse(200,{},"Upload successfully"))
 
--- ingestion-service/src/presentation/express/routes/upload-file.route.ts
+++ ingestion-service/src/presentation/express/routes/upload-file.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UploadFileController } from "../controllers/upload-file.controller";
 import { upload } from "../middlewares/upload-file.middleware";
+import FileProcessingService from "../../../services/fileProcessing.service";
 
 const router = Router();
-const controller = new UploadFileController();
+const fileProcessingService = new FileProcessingService();
+const controller = new UploadFileController(fileProcessingService);
 
 
 
